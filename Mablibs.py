4f693f945295e10bf33f8975ef5ee79a840275cf
VictorDolores64 <antonio.arteaga@uabc.edu.mx>
2023-05-01 19:07:21 -0700

    Proyecto 2 de python

---
+++ Mablibs.py
@@ -0,0 +1,11 @@
+# Variables
+nombre = input("Ingrese tu nombre: ")
+sustantivo = input("Ingrese un sustantivo: ")
+verbo = input("Ingrese un verbo: ")
+adjetivo = input("Ingrese un adjetivo: ")
+
+# Se crea una historia con los datos ingresados
+historia = f"{nombre} observó un {sustantivo} en la escuela y decidió {verbo} con él. Fue una experiencia {adjetivo} que nunca olvidará."
+
+#Muestra la historia
+print(historia)
