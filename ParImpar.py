dcb319b0e060679ba644e084232d7fe9f3f5e5e7
VictorDolores64 <antonio.arteaga@uabc.edu.mx>
2023-05-01 19:03:59 -0700

    Proyecto 1 de python

---
+++ ParImpar.py
@@ -0,0 +1,7 @@
+numero = int(input("Ingrese un número entero: "))
+
+# Valida si el número es par o impar
+if numero % 2 == 0:
+    print(numero, "es un número par.")
+else:
+    print(numero, "es un número impar.")
